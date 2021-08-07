e52847af6b87fb9bb56ba09e9b172e2a789ada29
Dmitrij Svetov <bandodok@gmail.com>
2021-08-07 12:03:17 +0300

    updated tests for downloading images testing

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,9 +1,34 @@
 from page_loader.page_loader import download
 import tempfile
+import pook
+from pytest import fixture
 
 
-def test_download():
+@fixture
+def get_response():
+    return open('tests/fixtures/easy_html.html', encoding="utf-8").read()
+
+
+@fixture
+def get_html():
+    return open('tests/fixtures/easy_html_images.html', encoding="utf-8").read()
+
+
+@pook.on
+def test_download(get_response, get_html):
     with tempfile.TemporaryDirectory() as tmpdir:
+        pook.get(
+            'https://ru.hexlet.io/courses',
+            reply=200,
+            response_body=get_response
+        )
+        pook.get(
+            'https://ru.hexlet.io/courses/assets/professions/nodejs.png',
+            reply=200,
+            response_body='ok'
+        )
         file_path = download('https://ru.hexlet.io/courses', tmpdir)
         temp_path = f'{tmpdir}/ru-hexlet-io-courses.html'
         assert file_path == temp_path
+        file = open(file_path, encoding="utf-8").read()
+        assert file == get_html
