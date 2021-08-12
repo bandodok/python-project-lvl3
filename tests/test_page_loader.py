cd35ccc63fdbbb308f34e578b0153358fa0a155c
Dmitrij Svetov <bandodok@gmail.com>
2021-08-12 21:56:10 +0300

    add error tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,15 +2,15 @@ import os
 from page_loader.page_loader import download
 import tempfile
 import pook
-from pytest import fixture
+import pytest
 
 
-@fixture
+@pytest.fixture
 def get_response():
     return open('tests/fixtures/easy_html.html', encoding="utf-8").read()
 
 
-@fixture
+@pytest.fixture
 def get_html():
     return open('tests/fixtures/easy_html_images.html', encoding="utf-8").read()
 
@@ -52,3 +52,61 @@ def test_download(get_response, get_html):
         assert os.path.exists(img_path), "img file not found"
         file = open(file_path, encoding="utf-8").read()
         assert file == get_html
+
+
+@pook.on
+def test_download_404(caplog):
+    pook.get(
+        'https://ru.hexlet.io/courses',
+        reply=404,
+    )
+    with pytest.raises(SystemExit):
+        download('https://ru.hexlet.io/courses')
+    assert '404 Client Error: Not Found for url: https://ru.hexlet.io/courses' in caplog.text
+    caplog.clear()
+
+
+@pook.on
+def test_download_500(caplog):
+    pook.get(
+        'https://google.com',
+        reply=500,
+    )
+    with pytest.raises(SystemExit):
+        download('https://google.com')
+    assert '500 Server Error: Internal Server Error for url: https://google.com' in caplog.text
+    caplog.clear()
+
+
+@pook.on
+def test_download_invalid_path(caplog):
+    pook.get(
+        'https://yandex.ru',
+        reply=200,
+    )
+    with pytest.raises(SystemExit):
+        download('https://yandex.ru', 'D:\\qwe')
+    assert 'Invalid path: D:\\qwe' in caplog.text
+    caplog.clear()
+
+
+def test_download_invalid_url(caplog):
+    with pytest.raises(SystemExit):
+        download('qwe')
+    assert "Invalid URL 'qwe': No schema supplied. Perhaps you meant http://qwe?" in caplog.text
+    caplog.clear()
+
+
+@pook.on
+def test_download_permissions_denied(caplog):
+    pook.get(
+        'https://ya.ru',
+        reply=200,
+    )
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = tmpdir + '/qwerty'
+        os.mkdir(path, mode=0o111)
+        with pytest.raises(SystemExit):
+            download('https://ya.ru', path)
+        assert 'PermissionError: Access denied: D:\\project\\qwerty/ya-ru_files\n' in caplog.text
+        caplog.clear()
